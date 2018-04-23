d5182e2b1c51baea59cf57b0c02720a4923f69c0
Shweta Shahane <shweta.shahane92@gmail.com>
2018-04-22 20:17:55 -0700

    Added Roles to Datatable

--- datatable/js/datatable.js
+++ datatable/js/datatable.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
+
 	$(".row_data").on('click', function(event) {
-	event.preventDefault(); 
-	if($(this).attr('edit_type') == 'button')
-	{
-		return false; 
-	}
+		event.preventDefault(); 
+		if($(this).attr('edit_type') == 'button')
+		{
+			return false; 
+		}
 
-	//make div editable
-	$(this).closest('div').attr('contenteditable', 'true');
-	//add bg css
-	$(this).addClass('bg-warning').css('padding','5px');
+		//get value of select tag
+		var person = $("#access option:selected").text();
 
-	$(this).focus();
-});
+		//make div editable 
+		if(person === "Admin" || person === "Professor"){
+			$(this).closest('div').attr('contenteditable', 'true');
+			$(this).addClass('bg-warning').css('padding','5px');
+			$(this).focus();
+		}else{
+			console.log("Student");
+		}				
+	});
 
 	//--->save single field data > start
 $(".row_data").on('focusout', function(event) 
@@ -27,21 +33,21 @@ $(".row_data").on('focusout', function(event)
 	var row_id = $(this).closest('tr').attr('row_id'); 
 	
 	var row_div = $(this)
-	.removeAttr('contenteditable') //make div editable		
-	.removeClass('bg-warning') //add bg css
+	.removeAttr('contenteditable') //make div uneditable		
+	.removeClass('bg-warning') //remove bg css 
 	.css('padding','')
-
-	var col_name = row_div.attr('col_name'); 
+	
+	var col_name = row_div.attr('colname'); 
 	var col_val = row_div.html(); 
 
 	var arr = {};
 	arr[col_name] = col_val;
 
-	//use the "arr"	object for your ajax call
+	//use the "arr"	object for ajax call
 	$.extend(arr, {row_id:row_id});
 
-	//out put to show
-	$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>');
+	//output to show
+	$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>')
 	
 });	
 //--->save single field data > end
