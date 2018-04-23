ef45d6be687543064003e70943f9b110c424c5ab
Shweta Shahane <shweta.shahane92@gmail.com>
2018-04-22 17:39:58 -0700

    datatable-save

--- datatable/js/datatable.js
+++ datatable/js/datatable.js
@@ -12,5 +12,37 @@ $(document).ready(function(){
 	$(this).addClass('bg-warning').css('padding','5px');
 
 	$(this).focus();
-})
+});
+
+	//--->save single field data > start
+$(".row_data").on('focusout', function(event) 
+{
+	event.preventDefault();
+
+	if($(this).attr('edit_type') == 'button')
+	{
+		return false; 
+	}
+
+	var row_id = $(this).closest('tr').attr('row_id'); 
+	
+	var row_div = $(this)
+	.removeAttr('contenteditable') //make div editable		
+	.removeClass('bg-warning') //add bg css
+	.css('padding','')
+
+	var col_name = row_div.attr('col_name'); 
+	var col_val = row_div.html(); 
+
+	var arr = {};
+	arr[col_name] = col_val;
+
+	//use the "arr"	object for your ajax call
+	$.extend(arr, {row_id:row_id});
+
+	//out put to show
+	$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>');
+	
+});	
+//--->save single field data > end
 });
