d0720bba0c9446716956fb6a9a9a10196c7afe16
Shweta Shahane <shweta.shahane92@gmail.com>
2018-04-24 00:23:49 -0700

    sorting in datatable

--- datatable/js/datatable.js
+++ datatable/js/datatable.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
-
 	$(".row_data").on('click', function(event) {
 		event.preventDefault(); 
-		if($(this).attr('edit_type') == 'button')
-		{
-			return false; 
-		}
 
 		//get value of select tag
 		var person = $("#access option:selected").text();
@@ -15,40 +10,69 @@ $(document).ready(function(){
 			$(this).closest('div').attr('contenteditable', 'true');
 			$(this).addClass('bg-warning').css('padding','5px');
 			$(this).focus();
-		}else{
-			console.log("Student");
-		}				
+		}			
 	});
 
 	//--->save single field data > start
-$(".row_data").on('focusout', function(event) 
-{
-	event.preventDefault();
-
-	if($(this).attr('edit_type') == 'button')
+	$(".row_data").on('focusout', function(event) 
 	{
-		return false; 
-	}
+		event.preventDefault();
 
-	var row_id = $(this).closest('tr').attr('row_id'); 
-	
-	var row_div = $(this)
-	.removeAttr('contenteditable') //make div uneditable		
-	.removeClass('bg-warning') //remove bg css 
-	.css('padding','')
-	
-	var col_name = row_div.attr('colname'); 
-	var col_val = row_div.html(); 
+		var row_id = $(this).closest('tr').attr('row_id'); 
+		
+		var row_div = $(this)
+		.removeAttr('contenteditable') //make div uneditable		
+		.removeClass('bg-warning') //remove bg css 
+		.css('padding','')
+		
+		var col_name = row_div.attr('colname'); 
+		var col_val = row_div.html(); 
 
-	var arr = {};
-	arr[col_name] = col_val;
+		var arr = {};
+		arr[col_name] = col_val;
 
-	//use the "arr"	object for ajax call
-	$.extend(arr, {row_id:row_id});
+		//use the "arr"	object for ajax call
+		$.extend(arr, {row_id:row_id});
 
-	//output to show
-	$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>')
-	
-});	
+		//output to show
+		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>')
+		
+	});	
 //--->save single field data > end
+
+// Sorting
+
+$('.sortable').click(function() {
+    var table = $(this).parents('table').eq(0);
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+    console.log(rows);
+    this.asc = !this.asc;
+    if (!this.asc) {
+    	rows = rows.reverse();
+    }
+    for (var i = 0; i < rows.length; i++){
+    	table.append(rows[i]);
+    }
 });
+
+});
+
+function getCellValue(row, index) { 
+	return $(row).children('td').eq(index).text(); 
+}
+
+function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index);
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
+    }
+}
+
+
+
+
+
+
+
+
+
